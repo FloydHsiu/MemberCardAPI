b2cb3f7e98083a68a860ca6ea398f1dabfaa5795
XIu-Chi Chang <s94040511s@gmail.com>
2016-10-12 21:05:19 +0800

    Update# index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ echo $acc.';'.$pwd;
 
 session_start();
 
-$result = $db->query("SELECT * FROM HCEproject.MEMBER WHERE ACCID = \'".$acc."\' and PWD =\'".$pwd."\'");
+$result = $db->query("SELECT * FROM HCEproject.MEMBER WHERE ACCID = '".$acc."' and PWD ='".$pwd."'");
 if ($result->num_rows > 0){
 	echo json_encode(array('valid' => true));
 	$_SESSION['valid']=true;
