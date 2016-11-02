c5e7bca4008e8bfd839265c54f7ba61199abfe36
XIu-Chi Chang <s94040511s@gmail.com>
2016-11-02 18:01:33 +0800

    Update# AddCard Client

---
+++ Client/addCard.php
@@ -0,0 +1,42 @@
+<?php
+
+require('../dbInfo.php');
+
+$db = new mysqli($hostname, $user, $pwd);
+
+session_start();
+
+if($_SESSION['valid'] === true){
+    $AccId = $_SESSION['id'];
+    $ComId = $_POST['ComId'];
+    $CardNum = $_POST['CardNum'];
+    $Phone = $_POST['Phone'];
+    $NationId = $_POST['NationId'];
+
+    $result = $db->query("select * from HCEproject.CARD where ComId=".$ComId." and CardNum=".$CardNum);
+
+    if($result->num_rows > 0){
+        $temp = $row->fetch_assoc();
+        if($temp['Phone'] === $Phone && strcmp($temp['NationId'], $NationId) === 0){
+            $update_sql = "UPDATE HCEproject.CARD SET AccId='".$AccId."' WHERE ComId=".$ComId." and CardNum=".$CardNum;
+            if($db->query($update_sql) === true){
+                echo json_encode(array('state'=> true));
+            }
+            else{
+                echo json_encode(array('state'=> 'Update error'));
+            }
+        }
+        else{
+            echo json_encode(array('state'=> 'Wrong card certificate'));
+        }
+    }
+    else{
+        echo json_encode(array('state'=> 'no this card'));
+    }
+}
+else{
+    echo json_encode(array('state'=> 'not login'));
+}
+
+
+?>
