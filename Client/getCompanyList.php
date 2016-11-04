cfc8995a0869bc3aa4673cb144df64f16065cb3d
XIu-Chi Chang <s94040511s@gmail.com>
2016-11-04 10:55:24 +0800

    Update# getCompanyList JSONobj and JSONarray type

--- Client/getCompanyList.php
+++ Client/getCompanyList.php
@@ -8,19 +8,23 @@
     if($_SESSION['valid'] === true){
         $result = $db->query("SELECT * FROM HCEproject.COMPANY");
 
-        $list = array();
+        $query_list = array();
+        $array_list = array();
 
         if($result->num_rows > 0){
             while($row = $result->fetch_assoc()){
-                // $temp = array(
-                //     'ComId'=> $row['ComId'], 
-                //     'ComName'=> $row['ComName']);
-                //array_push($list, $row['ComId']=> $row['ComName']);
-                $list[''.$row['ComId']] = $row['ComName'];
+                $temp = array(
+                    'ComId'=> $row['ComId'], 
+                    'ComName'=> $row['ComName']);
+                array_push($array_list, $temp);
+                $query_list[''.$row['ComId']] = $row['ComName'];
             }
         }
         
-        echo json_encode(array('CompanyList' => $list, 'state'=> 'success')); 
+        echo json_encode(array(
+            'CompanyList' => $query_list,
+            'CompanyList_array' => $array_list, 
+            'state'=> 'success')); 
     }
     else{
         echo json_encode(array('state'=> 'no login'));
