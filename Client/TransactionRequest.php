e7c44c5784ebc9ed30cb9b09099c8440050a9869
XIu-Chi Chang <s94040511s@gmail.com>
2016-11-22 11:45:49 +0800

    Update# debug

--- Client/TransactionRequest.php
+++ Client/TransactionRequest.php
@@ -3,21 +3,20 @@
 require('../dbInfo.php');
 require('../random.php');
 
+$db = new mysqli($hostname, $user, $pwd);
+
 session_start();
 
 function isCardAndAccMatch($ComId, $CardNum, $AccId){
-    $db = new mysqli($hostname, $user, $pwd);
-    $result = $db->query("SELECT * FROM HECproject.CARD WHERE ((ComId, CardNum) IN ( (".$ComId.",".$CardNum."))) and AccId=".$AccId);
+    global $db;
+    $result = $db->query("SELECT * FROM HCEproject.CARD WHERE ((ComId, CardNum) IN ( (".$ComId.",".$CardNum."))) and AccId=".$AccId);
     if($result->num_rows > 0){
         return true;
     }
     else{
         return false;
     }
-    $db->close();
 }
-
-$db = new mysqli($hostname, $user, $pwd);
 
 $ComId = $_POST['ComId'];
 $CardNum = $_POST['CardNum'];
@@ -33,7 +32,7 @@ if($_SESSION['valid']){
         echo json_encode(array('state'=>'success', 'transcode'=>$transcode));
     }
     else{
-        echo json_encode(array('state'=>'no card access', 'AccId'=>$AccId, 'ComId'=>$ComId, 'Cardnum'=>$CardNum));
+        echo json_encode(array('state'=>'no card access'));
     }
 }
 else{
